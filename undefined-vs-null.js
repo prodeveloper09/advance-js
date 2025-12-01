7407045f46bb79c09d433f11e86fd61c9e0f6be1
MD Imran <138973820+prodeveloper09@users.noreply.github.com>
2025-12-01 07:58:47 +0600

    undefined and null is clear

---
+++ undefined-vs-null.js
@@ -0,0 +1,12 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1+num2);
+ return
+}
+
+const info = {name:"md imran", phone:1944949678};
+console.log(info.address);
+
+let ages = [12, 18, 45];
+console.log(ages[80]);
