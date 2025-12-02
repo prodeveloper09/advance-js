6c8b12a7ef5ac98931290fbccaf02add8c357f82
MD Imran <138973820+prodeveloper09@users.noreply.github.com>
2025-12-02 10:53:37 +0600

    array-split

---
+++ scope.js
@@ -0,0 +1,11 @@
+function sum(first, second){
+    let result = first + second;
+    console.log(result);
+    if(result > 9){
+        let mood = "happy";
+        console.log(mood);
+    }
+}
+
+let output = sum(10, 120);
+// console.log(output);
